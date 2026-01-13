fb2dad6cb1a2b248a7c9c921e60a233e7151f453
rahul <rahulshakya9841@gamil.com>
2026-01-13 21:39:33 +0545

    postfix expression evaluated

--- prog_2.c
+++ prog_2.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
 #define MAX 50
 int top = -1;
 char stack[MAX];
 char postfixExp[100];
+int evalStack[MAX];
+int evalTop = -1;
 
 int isEmpty(){
 	return top == -1;
@@ -45,6 +49,48 @@ void display(){
 			printf("%c\n",stack[i]);
 		}
 	}
+}
+
+
+void pushEval(int value){
+    if(evalTop == MAX - 1){
+        printf("Eval Stack Overflow\n");
+    }
+    else{
+        evalStack[++evalTop] = value;
+    }
+}
+
+int popEval(){
+    if(evalTop == -1){
+        printf("Eval Stack Underflow\n");
+        return -1;
+    }
+    else{
+        return evalStack[evalTop--];
+    }
+}
+
+int evaluatePostfix(char* exp){
+    int i = 0;
+    while(exp[i] != '\0'){
+        if(isdigit(exp[i])){
+            pushEval(exp[i] - '0');
+        }
+        else{
+            int val1 = popEval();
+            int val2 = popEval();
+            switch(exp[i]){
+                case '+': pushEval(val2 + val1); break;
+                case '-': pushEval(val2 - val1); break;
+                case '*': pushEval(val2 * val1); break;
+                case '/': pushEval(val2 / val1); break;
+                case '^': pushEval((int)pow(val2, val1)); break;
+            }
+        }
+        i++;
+    }
+    return popEval();
 }
 
 int precedence(char c){
@@ -89,8 +135,9 @@ void checkExpression(char exp[]){
 }
 
 int main(){
-    char test[30] = "a+(b+c)";
+    char test[30] = "2+3*4-5^2/5"; // 2+12-25/5 = 14-5 = 9
     checkExpression(test);
     printf("Postfix Expression: %s\n", postfixExp);
+    printf("Evaluation Result: %d\n", evaluatePostfix(postfixExp));
     return 0;
 }
